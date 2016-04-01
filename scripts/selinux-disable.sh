687cb7df569b331b26dd291eef22710d6f3a068b
Been Kyung-yoon <ibin@best79.com>
2016-04-01 12:52:19 +0900

    수정### SELinux 를 비활성화합니다. ###
    /usr/sbin/setenforce: SELinux is disabled
    다음 명령이 실패하여, 설치가 중단되었습니다. (exit code: 1)
    # scripts/selinux-disable.sh

--- scripts/selinux-disable.sh
+++ scripts/selinux-disable.sh
@@ -9,4 +9,7 @@ source "${STACK_ROOT}/includes/function.inc.sh"
 title "SELinux 를 비활성화합니다."
 
 sed -i 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+
+# exit status code 를 1로 반환하여, 설치가 중단되므로 강제로 0으로 반환
 /usr/sbin/setenforce 0
+exit 0
