0a4a232bdf31a58341dc74d633e13128f5b91b88
Been Kyung-yoon <ibin@best79.com>
2016-10-02 11:21:34 +0900

    #9 php53 이 먼저 설치되므로, 링크까지 함께 걸어야 함

--- scripts/centos6-php53-install.sh
+++ scripts/centos6-php53-install.sh
@@ -46,4 +46,5 @@ if [ -f /etc/nginx/conf.d/0-php79.conf ]; then
 fi
 
 # #9 ioncube 설치를 위해 php53 경로가 필요하고, /usr/bin/php 는 php-cli-version-set.sh 에 의해 링크로만 존재하도록 함
-mv -f /usr/bin/php /usr/bin/php53
+mv -f /usr/bin/php /usr/bin/php53 \
+&& ln -s /usr/bin/php53 /usr/bin/php
