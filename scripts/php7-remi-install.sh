c71f811d60f256e97c6e46367cf25f4b728ab6d3
Been Kyung-yoon <ibin@best79.com>
2024-11-16 16:41:14 +0900

    #109 설치 PHP 모듈 stack.conf 에서 선언하도록 설정 분리 - PHP 버전별 모듈 차이 반영 및 별도 지정 지원 - chgrp, chown -v 를 -c 로 변경하여 변경된 경우만 출력

--- scripts/php7-remi-install.sh
+++ scripts/php7-remi-install.sh
@@ -3,25 +3,43 @@
 # Copyright:: Copyright (c) 2016 Been Kyung-yoon (http://www.php79.com/)
 # License:: The MIT License (MIT)
 
+# PHP 7.2 부터 mcrypt 확장은 pecl 로 이전
+
 STACK_ROOT=$( dirname $( cd "$( dirname "$0" )" && pwd ) )
 source "${STACK_ROOT}/includes/function.inc.sh"
 
-title "PHP [${1}] 버전을 설치합니다."
+PHP_VERSION=${1}
 
-if [ -z ${1} ]; then
+title "PHP [${PHP_VERSION}] 버전을 설치합니다."
+
+if [ -z ${PHP_VERSION} ]; then
   abort "설치할 PHP 버전을 입력하세요.  70, 71, 72, 74"
 fi
 
-yum_install php$1-php-cli php$1-php-fpm \
-php$1-php-common php$1-php-pdo php$1-php-mysqlnd php$1-php-mbstring php$1-php-mcrypt \
-php$1-php-opcache php$1-php-xml php$1-php-pecl-imagick php$1-php-gd php$1-php-fileinfo \
-php$1-php-pecl-mysql php$1-php-pecl-zip php$1-php-bcmath
+# stack.conf 에서 선언한 PHP 모듈 배열
+if [[ ${PHP_VERSION} = "70" || ${PHP_VERSION} = "71" ]]; then
+  PHP_MODULES=(${PHP_MODULES_70})
+else
+  PHP_MODULES=(${PHP_MODULES_72})
+fi
 
-if [ ! -f "/etc/opt/remi/php${1}/php.d/z-php79.ini" ]; then
-  notice "PHP 권장 설정이 추가되었습니다.\n설정 파일 경로) /etc/opt/remi/php${1}/php.d/z-php79.ini"
-  cp -av "${STACK_ROOT}/php/70/z-php79.ini" "/etc/opt/remi/php${1}/php.d/"
+# PHP 버전별 모듈명 완성  ex) php-pdo -> php80-php-pdo
+MERGE_PHP_MODULES=""
+for i in "${PHP_MODULES[@]}"
+do
+  MERGE_PHP_MODULES="${MERGE_PHP_MODULES} php${PHP_VERSION}-${i}"
+done
+
+yum_install php$1-php-cli \
+php$1-php-fpm \
+php$1-php-common \
+${MERGE_PHP_MODULES}
+
+if [ ! -f "/etc/opt/remi/php${PHP_VERSION}/php.d/z-php79.ini" ]; then
+  notice "PHP 권장 설정이 추가되었습니다.\n설정 파일 경로) /etc/opt/remi/php${PHP_VERSION}/php.d/z-php79.ini"
+  cp -av "${STACK_ROOT}/php/70/z-php79.ini" "/etc/opt/remi/php${PHP_VERSION}/php.d/"
   string_quote ${TIMEZONE}
-  sed -i "s/^date.timezone =.*/date.timezone = ${STRING_QUOTE}/g" "/etc/opt/remi/php${1}/php.d/z-php79.ini"
+  sed -i "s/^date.timezone =.*/date.timezone = ${STRING_QUOTE}/g" "/etc/opt/remi/php${PHP_VERSION}/php.d/z-php79.ini"
 fi
 
 PHP_FPM_CONF=/etc/opt/remi/php$1/php-fpm.d/www.conf
@@ -36,8 +54,8 @@ else
   sed -i 's/^listen = 127.0.0.1:9000/listen = 127.0.0.1:90'$1'/g' $PHP_FPM_CONF
 fi
 
-chgrp -v nobody /var/opt/remi/php$1/lib/php/*
-chown -v nobody /var/opt/remi/php$1/log/php-fpm
+chgrp -c nobody /var/opt/remi/php$1/lib/php/*
+chown -c nobody /var/opt/remi/php$1/log/php-fpm
 
 if [ "$SYSTEMCTL" = "1" ]; then
   systemctl enable php$1-php-fpm
@@ -49,7 +67,7 @@ fi
 
 # nginx 설치된 경우만 복사
 if [ -f /etc/nginx/conf.d/0-php79.conf ]; then
-  if [ ! -f "/etc/nginx/conf.d/1-fastcgi-php${1}.conf" ]; then
-    cp -av "${STACK_ROOT}/nginx/1-fastcgi-php${1}.conf" /etc/nginx/conf.d/
+  if [ ! -f "/etc/nginx/conf.d/1-fastcgi-php${PHP_VERSION}.conf" ]; then
+    cp -av "${STACK_ROOT}/nginx/1-fastcgi-php${PHP_VERSION}.conf" /etc/nginx/conf.d/
   fi
 fi
