377433f902dd77eb413d9c455a1e228ef2bb1336
Been Kyung-yoon <ibin@best79.com>
2024-11-16 19:31:51 +0900

    MariaDB, CentOS 7 에서 MaxScale 저장소 오류가 있어 제외

--- scripts/mariadb-repo-install.sh
+++ scripts/mariadb-repo-install.sh
@@ -10,9 +10,14 @@ title "MariaDB 저장소를 설치합니다."
 
 
 # 2023.11.23 CentOS 7 에서도 MariaDB 버전 선택 가능하도록 변경
-if [[ "$OS" = "centos7" || "$OS" = "rocky8" ]]; then
+if [ "$OS" = "rocky8" ]; then
   # https://mariadb.com/kb/en/mariadb-package-repository-setup-and-usage/
-  curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-${MARIADB_VERSION}"
+  curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | \
+  sudo bash -s -- --mariadb-server-version="mariadb-${MARIADB_VERSION}"
+elif [ "$OS" = "centos7" ]; then
+  # 2024-11-16, CentOS 7 에서 MaxScale 저장소 오류가 있어 제외
+  curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | \
+  sudo bash -s -- --mariadb-server-version="mariadb-${MARIADB_VERSION}" --skip-maxscale
 else
   # http://yum.mariadb.org/ 에서 공식 저장소 패키지가 없던 시기에 수동 설치 방법이지만, 하위 호환성 유지하여 기존 방법 유지
   if [ ! -f /etc/yum.repos.d/MariaDB.repo ]; then
